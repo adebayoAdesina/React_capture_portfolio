78b0a38ee169e32bf5d8d2bbc287ee5ecfd56d77
adebayoAdesina <adebayoadesina789@gmail.com>
2022-08-27 23:15:03 +0100

    structure added.

---
+++ src/components/faq_section.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const  FaqSection =() =>{
+    return (
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+            </div>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, iste.</p>
+            </div>
+
+            <div className="question">
+                <h4>Daily Schedue?</h4>
+            </div>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, iste.</p>
+            </div>
+
+            <div className="question">
+                <h4>Payment Method</h4>
+            </div>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, iste.</p>
+            </div>
+
+            <div className="question">
+                <h4>What Product Do You Offer?</h4>
+            </div>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, iste.</p>
+            </div>
+        </div>
+    );
+}
+
+export default FaqSection;
---
+++ src/components/pages/about_page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import AboutSection from '../about_section'
+import FaqSection from '../faq_section'
+import ServicesSection from '../services_section'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <AboutSection/>
+        <ServicesSection/>
+        <FaqSection/>
+    </div>
+  )
+}
+
+export default AboutPage
